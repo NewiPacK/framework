6b6403ca0c78bf770cb0e6034f5e8195c5b8a1c8
Your Name <you@example.com>
2024-10-17 15:33:13 +0500

    Create CsrfToken Method

--- core/Router.php
+++ core/Router.php
@@ -37,7 +37,7 @@ class Router
             'callback' => $callback,
             'middleware' => null,
             'method' => $method,
-            'needToken' => true,
+            'needCsrfToken' => true,
         ];
         return $this;
     }
@@ -82,6 +82,22 @@ class Router
                 &&
                 in_array($this->request->getMethod(), $route['method'])
             ) {
+                if (request()->isPost()) {
+                    if ($route['needCsrfToken'] && !$this->checkCsrfToken()) {
+                        if (request()->isAjax())  {
+                            echo json_encode([
+                               'status' => 'error',
+                               'data' => 'Security error',
+                            ]);
+                            die;
+                        } else {
+//                            session()->setFlash('error', 'Ошибка безопасности');
+//                            response()->redirect();
+                            abort('Page expired', 419);
+                        }
+                    }
+                }
+
                 foreach ($matches as $k => $v) {
                     if (is_string($k)) {
                         $this->route_params[$k] = $v;
@@ -91,6 +107,18 @@ class Router
             }
         }
         return false;
+    }
+
+    public function withoutCsrfToken(): self
+    {
+        $this->routes[array_key_last($this->routes)]['needCsrfToken'] = false;
+        return $this;
+    }
+
+    public function checkCsrfToken(): bool
+    {
+        return request()->post('csrf_token') && (request()->post('csrf_token') == session()->
+                get('csrf_token'));
     }
 
 }
