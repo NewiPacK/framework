5f8fe5db220a75d192ee37bed285879e401d200f
Your Name <you@example.com>
2024-10-24 23:21:05 +0500

    Create middleware, pagination

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -13,7 +13,7 @@
 
 <nav class="navbar navbar-expand-lg bg-dark mb-3" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" href="#">Logo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -23,10 +23,16 @@
                     <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">Home</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="<?= base_url('/dashboard'); ?>">Dashboard</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="<?= base_url('/register'); ?>">Register</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="<?= base_url('/login'); ?>">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="<?= base_url('/users'); ?>">Users</a>
                 </li>
             </ul>
         </div>
